03336da54a6745ed9600461588c9790744083eae
Mahesh Kumar <mkumar@videologygroup.com>
2018-06-03 13:45:35 -0400

    #NoIssue : Initial commit of the sprint boot skeleton project

---
+++ src/main/java/in/mkp/advertisers/AdvertisersApplication.java
@@ -0,0 +1,12 @@
+package in.mkp.advertisers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertisersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertisersApplication.class, args);
+	}
+}
---
+++ src/test/java/in/mkp/advertisers/AdvertisersApplicationTests.java
@@ -0,0 +1,16 @@
+package in.mkp.advertisers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdvertisersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
