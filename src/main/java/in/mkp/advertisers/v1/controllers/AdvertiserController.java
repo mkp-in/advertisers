5ace2053108d8b1e30776dfa2219385deaec80b6
Mahesh Kumar <mkumar@videologygroup.com>
2018-06-03 23:37:04 -0400

    #NoIssue : Added Post, Put and Delete

--- src/main/java/in/mkp/advertisers/v1/controllers/AdvertiserController.java
+++ src/main/java/in/mkp/advertisers/v1/controllers/AdvertiserController.java
@@ -1,9 +1,17 @@
 package in.mkp.advertisers.v1.controllers;
 
+import java.util.Optional;
+
 import in.mkp.advertisers.v1.entities.Advertiser;
 import in.mkp.advertisers.v1.repositories.AdvertiserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +23,33 @@ public class AdvertiserController {
     @Autowired
     private AdvertiserRepository advertiserRepository;
 
-    @GetMapping("/getOne")
-    public Advertiser getAdvertiser(@RequestParam(value = "name", required = true) final String name) {
-        return this.advertiserRepository.findAdvertiserByName(name);
+    @GetMapping("/get")
+    public Optional<Advertiser> getAdvertiser(@RequestParam(value = "id", required = true) final Integer id) {
+        return this.advertiserRepository.findById(id);
     }
+
+    @DeleteMapping("/delete")
+    public void deleteAdvertiser(@RequestParam(value = "id", required = true) final Integer id) {
+       this.advertiserRepository.deleteById(id);
+    }
+
+    @PostMapping("/insert")
+    public ResponseEntity<String> insertAdvertiser(@RequestBody final Advertiser advertiser) {
+        final Advertiser result = this.advertiserRepository.save(advertiser);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<String> updateAdvertiser(@RequestParam(value = "id", required = true) final Integer id, @RequestBody final Advertiser advertiser) {
+        Optional<Advertiser> result = this.advertiserRepository.findById(id);
+
+        if (!result.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        advertiser.setId(id);
+        this.advertiserRepository.save(advertiser);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
--- src/main/java/in/mkp/advertisers/v1/repositories/AdvertiserRepository.java
+++ src/main/java/in/mkp/advertisers/v1/repositories/AdvertiserRepository.java
@@ -6,6 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AdvertiserRepository extends JpaRepository<Advertiser, Integer> {
-
-    Advertiser findAdvertiserByName(String advertiserName);
 }
